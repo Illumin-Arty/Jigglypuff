b1f1e2cf7282e93fdbdebf6a421351f29e0553be
soulspark666 <spark_soul@mail.ru>
2022-09-19 00:21:52 +0530

    Remove Debug info

--- Venom_bot.py
+++ Venom_bot.py
@@ -34,7 +34,6 @@ async def join(ctx):
     if (ctx.author.voice): # If the person is in a channel
         channel = ctx.author.voice.channel
         await channel.connect()
-        await ctx.send('Bot joined')
     else: #But is (s)he isn't in a voice channel
         await ctx.send("You must be in a voice channel first so I can join it.")
 
@@ -42,7 +41,6 @@ async def join(ctx):
 async def leave(ctx): # Note: ?leave won't work, only ?~ will work unless you change  `name = ["~"]` to `aliases = ["~"]` so both can work.
     if (ctx.voice_client): # If the bot is in a voice channel 
         await ctx.guild.voice_client.disconnect() # Leave the channel
-        await ctx.send('Bot left')
     else: # But if it isn't
         await ctx.send("I'm not in a voice channel, use the join command to make me join")
 
